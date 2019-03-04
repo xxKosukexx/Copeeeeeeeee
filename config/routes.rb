b6468f2e1862e8e3f2b08b36e4063827b6c49ce8
kosuke kageyama <kosuke@kosuke-no-MacBook-Air.local>
2019-03-04 21:51:56 +0900

    20190304

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,28 @@
 Rails.application.routes.draw do
-  get 'service/info'
+
+
   resources :commands
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #get
+  get 'users/add'
+  get 'users/destroy'
+  get 'users/twitter_add'
+  get 'users/twitter_destroy'
+  get 'authentication_management/login'
+  get 'authentication_management/logout'
+  get 'authentication_management/entrance_screen'
+  get 'categories/add'
+  get 'categories/destroy'
+  get 'categories/edit'
+  get 'command_management/show'
+  get 'command_management/clipboard'
+  get 'service_info/info'
+  get 'take_in_command_with_file/file_select'
+  get 'take_in_command_with_file/teke_in_command'
+
+  #post
+
+
+  root to: 'authentication_management#entrance_screen'
 end
